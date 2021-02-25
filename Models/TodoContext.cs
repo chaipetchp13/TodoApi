9f6e801096a75d4d5b30eb4d175218e35c240bd2
Kittiphon Chaipetch <77887701+chaipetchp13@users.noreply.github.com>
2021-02-25 22:05:53 +0700

    commit

--- Models/TodoContext.cs
+++ Models/TodoContext.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
 namespace TodoApi.Models
 {
-    public class TodoContext
+    public class TodoContext : DbContext
     {
-        
+       public TodoContext(DbContextOption<TodoContext> options)
+        :base(options)
+        {
+
+        }
+        public DbSet<TodoItem> TodoItems {get;set;}
     }
 }
