f715de4506d49781fff90107cb3c00fa01e5d6fb
Kittiphon Chaipetch <77887701+chaipetchp13@users.noreply.github.com>
2021-02-26 14:31:17 +0700

    commmit

--- Models/TodoItemDTO.cs
+++ Models/TodoItemDTO.cs
@@ -3,8 +3,8 @@ namespace TodoApi.Models
 {
     public class TodoItemDTO
     {
-        public TodoItemDTO()
-        {
-        }
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
     }
 }
